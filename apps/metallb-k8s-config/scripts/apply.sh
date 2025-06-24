3b6f38e0480b87e53f7e48fc0d780f09534be631
Francescoeman <francescoemanuele32@gmail.com>
2025-06-24 15:34:59 +0200

    added metallb to app

--- apps/metallb-k8s-config/scripts/apply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Apply MetalLB manifests
-kubectl apply -f ../manifests/metallb-namespace.yaml
-kubectl apply -f ../manifests/metallb-secret.yaml
-kubectl apply -f ../manifests/metallb-configmap.yaml
-kubectl apply -f ../manifests/metallb-deployment.yaml
-
-echo "MetalLB has been successfully applied to the Kubernetes cluster."
